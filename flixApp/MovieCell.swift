4b19a37ad66c3686a466c2437f2ff0ce6e09425e
shreejalluitel <luitelshreejal@berkeley.edu>
2020-10-23 23:30:37 -0700

    finished flixApp; alamofireImage doesn't work

---
+++ flixApp/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flixApp
+//
+//  Created by Shreejal Luitel on 10/23/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
