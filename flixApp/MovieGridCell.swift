f3615639f7048db541dd9ec9371314fda3148e7d
shreejalluitel <luitelshreejal@berkeley.edu>
2020-10-30 17:58:40 -0700

    finished app

---
+++ flixApp/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixApp
+//
+//  Created by Shreejal Luitel on 10/30/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
